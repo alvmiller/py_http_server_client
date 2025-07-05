e612034e14837426d270bcb9bcf8ccd96f05685a
alvmiller <134068320+alvmiller@users.noreply.github.com>
2025-07-05 22:54:43 +0300

    Create server_docs.py

---
+++ client/server/server_docs.py
@@ -0,0 +1,10 @@
+def get_info():
+  '''Simple Naive Py HTTP Server Example'''
+  return None
+
+print()
+print("Using __doc__:")
+print(get_info.__doc__)
+print("Using help:")
+help(get_info) 
+print()
